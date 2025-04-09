c4db63c96f8a86d598eb147b4bfe5a17918b83a9
Fabricio <Fabriciogarcia20@gmail.com>
2025-04-08 21:46:03 -0300

    all

--- src/main/java/com/crud/alpha/clase/Usuario.java
+++ src/main/java/com/crud/alpha/clase/Usuario.java
@@ -3,19 +3,22 @@ package com.crud.alpha.clase;
 import com.crud.alpha.enums.Tipo_Usuario;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.envers.Audited;
 
 
 //Modelado de la clase y tabla usuarios, @Tabla le da el nombre a la tabla, los demas @ nos ahorran declarar los getters
 //Setters y constructor vacio y con argumentos.
 // el @Id establece que atributo va a ser la clave primaria y el Identity hace que cada nuevo ID autogenerado sea el siguiente del anterior.
 
+
 @Entity
+@Audited
 @Table(name = "usuarios")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario {
+public class Usuario{
 // Importante no nombrar atributos utilizando '_' (piso/barra baja)
 
     @Id
@@ -37,4 +40,7 @@ public class Usuario {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Tipo_Usuario tipo;
+
+
+
 }
