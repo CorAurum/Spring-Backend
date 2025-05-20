89f924688d690201a0f710e2174104f01aa9a0fb
Joaquín <joaquingomezleites@gmail.com>
2025-05-20 09:52:00 -0300

    vendedor fix

--- src/main/java/com/crud/alpha/repository/VendedorRepository.java
+++ src/main/java/com/crud/alpha/repository/VendedorRepository.java
@@ -1,24 +1,19 @@
-//package com.crud.alpha.repository;
-//
-//import com.crud.alpha.clase.Usuarios.Cliente.Cliente;
-//import com.crud.alpha.clase.Usuarios.Vendedor.Vendedor;
-//import com.crud.alpha.enums.Beneficiario;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Repository
-//public interface VendedorRepository extends JpaRepository<Vendedor, Long> {
-//    Optional<Vendedor> findByEmail(String email); //Metodo generado usando la funcion de JPA findBy
-//
-//    Optional<Vendedor> findByClerkId(String clerkId); //Metodo generado usando la funcion de JPA findBy
-//
-//    List<Vendedor> findBytipoBenef(Beneficiario tipoBenef); //Metodo generado usando la funcion de JPA findBy
-//
-//    boolean existsByEmail(String email); //Metodo generado usando la funcion de JPA existsBy
-//
-//    boolean existsByClerkId(String clerkId); //Metodo generado usando la funcion de JPA existsBy
-//}
-//
+package com.crud.alpha.repository;
+
+import com.crud.alpha.clase.Usuarios.Vendedor.Vendedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VendedorRepository extends JpaRepository<Vendedor, Long> {
+    Optional<Vendedor> findByEmail(String email); //Metodo generado usando la funcion de JPA findBy
+
+    Optional<Vendedor> findByClerkId(String clerkId); //Metodo generado usando la funcion de JPA findBy
+
+    boolean existsByEmail(String email); //Metodo generado usando la funcion de JPA existsBy
+
+    boolean existsByClerkId(String clerkId); //Metodo generado usando la funcion de JPA existsBy
+}
+
